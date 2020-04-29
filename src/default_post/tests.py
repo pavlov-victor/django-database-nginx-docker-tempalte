c599c4dca915d686f747a6e00132f3328720c577
Pavlov Victor <vitek@goykt.ru>
2020-04-29 21:46:16 +0900

    Поправил стандартный текст

--- src/default_post/tests.py
+++ src/default_post/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from .models import DefaultPost
 
+
 class DefaultPostTest(TestCase):
+    test_post_text = 'Тестовый пост'
 
     def test_create_post(self):
-        new_post = DefaultPost(title='Тестовый пост', description='Тестовое описание')
+        new_post = DefaultPost(title=self.test_post_text, description='Тестовое описание')
         new_post.save()
-        taked_new_post = DefaultPost.objects.first()
-        assert taked_new_post.title == 'Тестовый пост'
+        assert DefaultPost.objects.first().title == self.test_post_text
